1a78bc3126ff71f364d7dc9a84f49d9fd472349a
Michel12333 <pzqfranz@gmail.com>
2024-08-01 23:10:12 +1000

    update the Fit5032 week1-week2 efilo

---
+++ MONASH_IT_MASTER/FIT5032Internet applications development/fit5032-week2/zpei-library/src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import JSON from './components/JSON.vue';
+</script>
+
+<template>
+  <JSON></JSON>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
