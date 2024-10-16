4d291c32d6a576ab57544c58ded04010e81c118e
Franz-Pei <pzqfranz@gmail.com>
2024-10-16 14:29:25 +1100

    update the vite.config.js

--- MONASH_IT_MASTER/FIT5032Internet_applications_development/fit5032-week11/zpei-library/vite.config.js
+++ MONASH_IT_MASTER/FIT5032Internet_applications_development/fit5032-week11/zpei-library/vite.config.js
@@ -3,8 +3,10 @@ import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import { fileURLToPath, URL } from 'node:url';
 
+
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
+  base: '/Franz/', // Directly set your base URL here
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
